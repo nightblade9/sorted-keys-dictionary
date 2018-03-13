f207d10a5f11357892e6c6d73a2cb92be1eb19e7
nightblade9 <nightbladecodes@gmail.com>
2018-03-13 16:39:53 -0400

    Add failing slicing tests. This is too much work to continue!

--- sorted_keys_dictionary/test/test_sorted_keys_dictionary.py
+++ sorted_keys_dictionary/test/test_sorted_keys_dictionary.py
@@ -62,3 +62,39 @@ class TestSortedKeysDictionary:
         s = SortedKeysDictionary({"one": 1, "two": 2, "three": 3})
         s.set("two", 22)
         assert len(s) == 3
+
+    def test_class_is_sequence(self): pass # TODO: fill in at the end
+
+    def test_index_slicing_returns_items(self):
+        s = SortedKeysDictionary({"one": 1, "two": 2, "three": 3})
+        assert s[0] == "one"
+        assert s[1] == "two"
+    
+    def test_positive_slicing_returns_subset(self):
+        s = SortedKeysDictionary({"one": 1, "two": 2, "three": 3})
+        actual = s[0:1]
+        assert len(actual) == 1
+        assert actual[0] == "one"
+    
+    def test_partial_slicing_returns_subset(self):
+        s = SortedKeysDictionary({"one": 1, "two": 2, "three": 3, "four": 4})      
+        suffix = s[:2]
+        assert len(suffix) == 2
+        assert suffix[0] == "one"
+        assert suffix[1] == "two"
+
+        prefix = s[2:]
+        assert len(prefix) == 2
+        assert prefix[0] == "three"
+        assert prefix[1] == "four"
+    
+    def test_slicing_returns_copy_without_indicies(self):
+        s = SortedKeysDictionary({"one": 1, "two": 2, "three": 3, "four": 4})
+        actual = s[:]
+        assert len(actual) == 4
+        assert actual[0] == "one"
+        assert actual[1] == "two"
+        assert actual[2] == "three"
+        assert actual[3] == "four"
+        
+        
