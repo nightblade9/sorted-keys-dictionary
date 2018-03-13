49c8a3c55e0640f0dbe22c12e4d8202ae4e2c6f5
nightblade9 <nightbladecodes@gmail.com>
2018-03-13 15:34:02 -0400

    Basic getter/setter

--- sorted_keys_dictionary/sorted_keys_dictionary.py
+++ sorted_keys_dictionary/sorted_keys_dictionary.py
@@ -4,3 +4,9 @@ class SortedKeysDictionary:
             self._data = dict(dictionary)
         else:
             self._data = {}
+
+    def get(self, key):
+        return self._data[key]
+    
+    def set(self, key, value):
+        self._data[key] = value
--- sorted_keys_dictionary/test/test_sorted_keys_dictionary.py
+++ sorted_keys_dictionary/test/test_sorted_keys_dictionary.py
@@ -10,3 +10,19 @@ class TestSortedKeysDictionary:
         s2 = SortedKeysDictionary(d)
         assert s2._data == d
         assert s2._data is not d # it's a copy, not a reference
+
+    def test_getter_gets_initializer_values(self):
+        # Don't use indexing because it might not be implemented yet
+        s = SortedKeysDictionary({"name": "test", "age": 271})
+        assert s.get("name") == "test"
+        assert s.get("age") == 271
+    
+    def test_getter_gets_set_values_overriding_previous(self):
+        s = SortedKeysDictionary()
+        s.set("monkeys", 37)
+        s.set("apples", "seven")
+        assert s.get("monkeys") == 37
+        assert s.get("apples") == "seven"
+
+        s.set("monkeys", 99)
+        assert s.get("monkeys") == 99
